dd0c9c24a08f1b04b9a5dbbdefe5f49424cd3ddf
sleko01 <svenleko2@gmail.com>
2022-10-29 12:12:54 +0200

    Project setup

---
+++ src/main/java/Primavara/rest/CuvariPasaApplication.java
@@ -0,0 +1,13 @@
+package Primavara.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuvariPasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuvariPasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Primavara/rest/CuvariPasaApplicationTests.java
@@ -0,0 +1,13 @@
+package Primavara.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuvariPasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
