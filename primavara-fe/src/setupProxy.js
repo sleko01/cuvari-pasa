86fd80688af1de2708f09996dfec5bff00597823
unknown <ik52979@fer.hr>
2022-11-17 10:17:11 +0100

    U frontend dodani /api u login i register i proxy prepravljen na online adress

--- primavara-fe/src/setupProxy.js
+++ primavara-fe/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080/",
+      target: "https://cuvari-pasa-be.onrender.com",
       changeOrigin: true,
     })
   );
--- primavara-fe/src/views/register.js
+++ primavara-fe/src/views/register.js
@@ -56,7 +56,7 @@ function Register(){
     function onSubmit(e) {
         e.preventDefault()
         // alert("Tu trebas implementirat vezu frontenda i bekenda")
-        axios.post('/users/register', {
+        axios.post('/api/users/register', {
             "username": form.username,
             "firstName": form.firstName,
             "lastName": form.lastName,
