6f9aaeb4f7770cdee92b9f9d2038e093b78718e9
unknown <ik52979@fer.hr>
2022-11-17 14:48:00 +0100

    Promijenjen setupProxy na default

--- primavara-fe/src/setupProxy.js
+++ primavara-fe/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://cuvari-pasa-be.onrender.com",
+      target: "http://localhost:8080/",
       changeOrigin: true,
     })
   );
