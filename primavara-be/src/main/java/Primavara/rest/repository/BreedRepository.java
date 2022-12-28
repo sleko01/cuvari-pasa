1f2a0abaeac27384b209573359530186195efd3c
Antonio Lukic <antonio.lukic@fer.hr>
2022-12-28 13:35:22 +0100

    Priprema za spajanje sa frontend_oglasi

--- primavara-be/src/main/java/Primavara/rest/repository/BreedRepository.java
+++
@@ -1,9 +0,0 @@
-package Primavara.rest.repository;
-
-import Primavara.rest.domain.Breed;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BreedRepository extends JpaRepository<Breed, Long> {
-    Integer countByBreedId(Long id);
-    Breed findByBreedId(Long id);
-}
